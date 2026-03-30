05fc50ad623a6fc605340e2f3ad03ca8d7f855b6
haolei521 <863027237@qq.com>
2026-03-30 14:01:20 +0800

    add synv

--- build/bag_reading/CMakeFiles/bag_reading_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bag_reading_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bag_reading_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/bag_reading/CMakeFiles/bag_reading_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for bag_reading_uninstall.
